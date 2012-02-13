eae41bcf384de9cbfbd787c39a9babffbe92c18d
alexis.mignon <alexis.mignon@d7bc61e3-b3af-4758-e35e-9020d0da9e97>
2012-02-13 12:11:49 +0000

    added setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = "pypca",
+    version = "0.1",
+    description = "Modules for Principal Component Analysis",
+    author = "Alexis Mignon",
+    author_email = "alexis.mignon@gmail.com",
+    url = "https://code.google.com/p/pypca/",
+    py_modules = ["PCA"],
+)
